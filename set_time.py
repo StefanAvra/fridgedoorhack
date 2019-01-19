f9fadc856fee6ce154cad61466d3e53eb89ef1df
avra <stefan.avra@gmail.com>
2019-01-19 22:27:01 +0100

    retry syncing ntp time if failed

--- set_time.py
+++ set_time.py
@@ -10,5 +10,10 @@ async def keep_time_synced():
     # RTC in ESP8266 has very bad accuracy, drift may be seconds per minute.
     # Due to limitations of the ESP8266 chip the internal real-time clock (RTC) will overflow every 7:45h.
     while True:
-        ntptime.settime()
+        try:
+            ntptime.settime()
+        except OSError as e:
+            print('Time could not be synced: {}\nRetrying in 1 min'.format(e))
+            await uasyncio.sleep(60)
+            ntptime.settime()
         await uasyncio.sleep(3600)
