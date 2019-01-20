bb241b578329839469b6d3ed1f6b840a66c21ca0
avra <stefan.avra@gmail.com>
2019-01-20 14:19:42 +0100

    added more settings

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-BUTTON_PIN = 14
-BUZZER_PIN = 12
-ENABLE_LOG = True
-LEGIT_OPEN_TIME = 10  # how many seconds it is ok to keep the fridge open
+BUTTON_PIN = 14  # pin for door switch
+BUZZER_PIN = 12  # pin for piezo buzzer
+ENABLE_LOG = True  # logs the events to log/door.log
+LEGIT_OPEN_TIME = 20  # how many seconds until the alarm goes off
+CLOSING_SOUND = True  # enable a confirmation sound for closing the door
+ONE_UP = True  # play an achievement sound if the door was opened for COUNTER_MAX times
+COUNTER_MAX = 100  # see above
